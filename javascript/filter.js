f5433f92d1b08caf94be7dd4e118c2f045fe6ac0
ryang <ryan.r.c.339ac@gmail.com>
2021-10-20 08:35:04 -0300

    now the filter works fine!

---
+++ javascript/filter.js
@@ -0,0 +1,50 @@
+const filter = () => {
+    
+    const filter = document.querySelector(".filter")
+    
+    const projs = document.querySelectorAll(".filter__proj")
+    
+    const siteProj  = document.querySelectorAll(".proj--sites")
+    const jsProj    = document.querySelectorAll(".proj--js")
+    const reactProj = document.querySelectorAll(".proj--react")
+    
+    
+    
+    projs.forEach(element => {
+        element.addEventListener('click', ()=>{
+            const filterInScrean = element.getAttribute('filter')
+
+            
+            siteProj.forEach(disappear => {
+
+                if ( String(siteProj[0].className).includes(filterInScrean) ){
+                    disappear.style.display  = 'flex'
+                } 
+                else{
+                    disappear.style.display  = 'none'
+                }
+            })
+            jsProj.forEach(disappear => {
+
+                if ( String(jsProj[0].className).includes(filterInScrean) ){
+                    disappear.style.display  = 'flex'
+                } 
+                else{
+                    disappear.style.display  = 'none'
+                }
+            })
+            reactProj.forEach(disappear => {
+
+                if ( String(reactProj[0].className).includes(filterInScrean) ){
+                    disappear.style.display  = 'flex'
+                } 
+                else{
+                    disappear.style.display  = 'none'
+                }
+            })
+            
+
+        })
+    })
+}
+filter()

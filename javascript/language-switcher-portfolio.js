6e1acddd7685fb442e94388569d50571a19128ad
ryang <ryan.r.c.339ac@gmail.com>
2021-11-05 07:20:06 -0300

    (freelancer) added the spanish text

--- javascript/language-switcher-portfolio.js
+++ javascript/language-switcher-portfolio.js
@@ -191,7 +191,9 @@ const data =
      "reactNoteTitle":
                  "Administrador de notas, React",
      "reactNoteContent":
-                 "Un administrador de notas de la aplicación de reacción, guarda la última nota del usuario en localStorage y maneja las fechas. No solo consume 2 API: localStorage, API de navegadores y Moment.js, API externa, sino que usa React también, biblioteca de JavaScript de Facebook."
+                 "Un administrador de notas de la aplicación de reacción, guarda la última nota del usuario en localStorage y maneja las fechas. No solo consume 2 API: localStorage, API de navegadores y Moment.js, API externa, sino que usa React también, biblioteca de JavaScript de Facebook.",
+    "freelaContent":
+                 "Esa es mi página web para autónomos. En este proyecto, no solo demuestro mis habilidades como Front End Dev usando JavaScript y animación CSS para hacer un sitio casi en vivo, sino también un diseño de alto nivel. Estoy muy orgulloso de este repositorio."
     },
     "Italiano":
     {"portfolio":

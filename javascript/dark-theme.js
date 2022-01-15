d9fa2f3027ff09901af935d5187493768ae5a4f9
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-15 14:08:07 -0300

    now if you click in the dark theme it is saved into localStorage

--- javascript/dark-theme.js
+++ javascript/dark-theme.js
@@ -3,9 +3,18 @@ Dark-Theme
 ==========*/
 const darkTheme = () => {
     const html = document.querySelector("html")
+
+    
+    const isDarkMode = localStorage.getItem('isDarkMode');
+    if(isDarkMode == "true"){
+        html.classList.add('dark-theme')
+    }
+    
     const checkbox = document.querySelector("#switch")
     checkbox.addEventListener('change', function(){
-            html.classList.toggle('dark-theme')
+        html.classList.toggle('dark-theme')
+        console.log(html.classList.contains("dark-theme"))
+        localStorage.setItem('isDarkMode', html.classList.contains("dark-theme"));
     })
 }
 darkTheme()
